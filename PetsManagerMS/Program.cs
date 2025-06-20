117009cd1afc19ae7dd11d20d93ec42851761534
simonky <simonasb19@gmail.com>
2025-06-20 13:40:36 -0400

    feat: Configuracion Inicial

--- PetsManagerMS/Program.cs
+++ PetsManagerMS/Program.cs
@@ -1,13 +1,64 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Models.Database;
+using PetsManagerMS.Services;
+using Shared;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllersWithViews().AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.Converters.Add(new StringTrimmerJsonConverter());
+    })
+    .ConfigureApiBehaviorOptions(opt =>
+    {
+        IActionResult Data(ActionContext context)
+        {
+            ProblemDetailsFactory? problemDetailsFactory = context.HttpContext.RequestServices.GetRequiredService<ProblemDetailsFactory>();
+            ValidationProblemDetails problemDetails = problemDetailsFactory.CreateValidationProblemDetails(context.HttpContext, context.ModelState, 422);
+            return new ObjectResult(problemDetails) { StatusCode = 422 };
+        }
 
-builder.Services.AddControllers();
+        opt.InvalidModelStateResponseFactory = Data;
+    });
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+
+
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseOracle(builder.Configuration.GetConnectionString("db"));
+});
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("dev",
+        policy =>
+        {
+            policy
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+
+        });
+});
+
+
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseCors("dev");
+}
 
+app.UseExceptionHandler();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
